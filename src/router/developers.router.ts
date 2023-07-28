74cb6b1516d05e7e8cd2401a923595829a64e1f0
aleviannaf <alexandrevianna511@gmail.com>
2023-07-28 17:23:16 -0400

    feat: new router to infos update

--- src/router/developers.router.ts
+++ src/router/developers.router.ts
@@ -8,6 +8,18 @@ developersRouter.post("", middlewares.uniqueEmail, developersControllers.create)
 developersRouter.get("", developersControllers.retrieve)
 developersRouter.get("/:id", middlewares.checkIdDeveloper, developersControllers.retrieveForId)
 developersRouter.post("/:id/infos", middlewares.checkIdDeveloper, developersControllers.createInfos)
-developersRouter.patch("/:id", middlewares.checkIdDeveloper, middlewares.uniqueEmail, developersControllers.updateDeveloper)
+
+developersRouter.patch(
+    "/:id", 
+    middlewares.checkIdDeveloper, 
+    middlewares.uniqueEmail, 
+    developersControllers.updateDeveloper
+)
+
+developersRouter.patch(
+    "/:id/infos", 
+    middlewares.checkIdDeveloper,
+    developersControllers.updateInfo
+)
 
 export default developersRouter
