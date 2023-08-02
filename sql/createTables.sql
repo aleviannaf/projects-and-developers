5bd4f058d1073bbad47a5d539ead6d5004d247a0
aleviannaf <alexandrevianna511@gmail.com>
2023-08-01 21:27:23 -0400

    update: update in table projects

--- sql/createTables.sql
+++ sql/createTables.sql
@@ -16,15 +16,15 @@ create table if not EXISTS developers(
 
 
 CREATE TABLE IF NOT EXISTS projects (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(50) NOT NULL,
-    description TEXT NOT NULL,
-    estimatedTime VARCHAR(20) NOT NULL,
-    repository VARCHAR(120) NOT NULL,
-    startDate TIMESTAMP NOT NULL,
-    endDate TIMESTAMP,
-    developerId INTEGER,
-    FOREIGN KEY (developerId) REFERENCES developers (id) ON DELETE SET NULL
+    "id" SERIAL PRIMARY KEY,
+    "name" VARCHAR(50) NOT NULL,
+    "description" TEXT NOT NULL,
+    "estimatedTime" VARCHAR(20) NOT NULL,
+    "repository" VARCHAR(120) NOT NULL,
+    "startDate" TIMESTAMP NOT NULL,
+    "endDate" TIMESTAMP,
+    "developerId" INTEGER,
+    FOREIGN KEY ("developerId") REFERENCES developers (id) ON DELETE SET NULL
 );
 
 
@@ -44,9 +44,9 @@ create table if not exists "technologies"(id SERIAL primary key, name "tech" not
 
 CREATE TABLE IF NOT EXISTS projects_technologies (
     id SERIAL PRIMARY KEY,
-    projectId INTEGER NOT NULL,
-    technologyId INTEGER NOT NULL,
-    addedIn TIMESTAMP NOT NULL,
-    FOREIGN KEY (projectId) REFERENCES projects (id) ON DELETE CASCADE,
-    FOREIGN KEY (technologyId) REFERENCES technologies (id) ON DELETE CASCADE
+    "projectId" INTEGER NOT NULL,
+    "technologyId" INTEGER NOT NULL,
+    "addedIn" TIMESTAMP NOT NULL,
+    FOREIGN KEY ("projectId") REFERENCES projects (id) ON DELETE CASCADE,
+    FOREIGN KEY ("technologyId") REFERENCES technologies (id) ON DELETE CASCADE
 );
