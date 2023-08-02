ad024347baaf617de58785ea572c410b65240497
aleviannaf <alexandrevianna511@gmail.com>
2023-08-01 21:28:09 -0400

    delete: delete util transformsData

--- src/utils/transformsData.utils.ts
+++
@@ -1,9 +0,0 @@
-const transformarDataUtils = (data: string) => {
-    
-    const partes = data.split('/')
-    const newData = `${partes[2]}-${partes[1].padStart(2, '0')}-${partes[0].padStart(2, '0')}`
-
-    return newData;
-}
-
-export default  transformarDataUtils 
